e144e2b7584ea3482e07e6ff9f772e2bf4e3a1b9
= <mohammadmahdisimin@gmail.com>
2026-03-02 21:51:35 +0330

    add difficulty constants

--- entity/game.go
+++ entity/game.go
@@ -4,5 +4,13 @@ type Game struct {
 	ID           uint8
 	CategoryID   uint8
 	QuestionList []int8
-	difficulty   uint8
+	Difficulty   GameDifficulty
 }
+
+type GameDifficulty uint8
+
+const (
+	GameDifficultyEasy GameDifficulty = iota + 1
+	GameDifficultyMedium
+	GameDifficultyHard
+)
--- entity/question.go
+++ entity/question.go
@@ -3,5 +3,14 @@ package entity
 type Question struct {
 	ID         uint8
 	CategoryID uint8
+	Difficulty QuestionDifficulty
 	Text       string
 }
+
+type QuestionDifficulty uint8
+
+const (
+	QuestionDifficultyEasy QuestionDifficulty = iota + 1
+	QuestionDifficultyMedium
+	QuestionDifficultyHard
+)
